2df9d238718ab36cafcb63340b7d8009fef64105
ArtemiiKolomiichuk <artemii.kolomiichuk@ukma.edu.ua>
2023-04-05 16:03:49 +0300

    Remove redundancy in entities

--- Assets/Scripts/Entities.cs
+++ Assets/Scripts/Entities.cs
@@ -1,16 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Entities
 {
-    public enum ColumnType
-    {
-        Editable = 0,
-        FK,
-        ReadOnly 
-    }
-
     public enum CellType
     {
         InputField = 0,
@@ -22,11 +13,6 @@ namespace Entities
     {
         public static readonly int dimensions;
         public abstract List<string> ToList();
-        public virtual string OrderQuery(string attr, bool desc = false)
-        {
-            return null;
-        }
-        public abstract List<ColumnType> ColumnTypes();
         public static List<CellType> CellTypes()
         {
             return null;
@@ -41,7 +27,6 @@ namespace Entities
         public string charachteristics { get; set; }
         public string manufacturer { get; set; }
         public int category_number { get; set; }
-        
         public int discounted { get; set; }
         public decimal base_price { get; set; }
         public decimal real_price { get; set; }
@@ -50,25 +35,9 @@ namespace Entities
         {
             return $"Product: \n\aid_product: {id_product}, \n\aproduct_name: {product_name}, \n\amanufacturer: {manufacturer}, \n\acharacteristics: {charachteristics}, \n\acategory_number: {category_number}, \n\adiscounted: {discounted}, \n\abase_price: {base_price}, \n\areal_price: {real_price}";
         }
-
         public override List<string> ToList()
         {
             return new List<string> { id_product.ToString(), product_name, charachteristics, manufacturer, category_number.ToString(), discounted.ToString(), base_price.ToString(), real_price.ToString() };
-        }
-
-        public static new string OrderQuery(string attr, bool desc = false)
-        {
-            return @$"
-            SELECT 
-                *
-            FROM
-                Product
-            ORDER BY {attr} {(desc ? "DESC" : "ASC")}";
-        }
-
-        public override List<ColumnType> ColumnTypes()
-        {
-            return new List<ColumnType> { ColumnType.Editable, ColumnType.Editable, ColumnType.Editable, ColumnType.Editable, ColumnType.FK, ColumnType.Editable, ColumnType.Editable, ColumnType.ReadOnly };
         }
         public static new List<CellType> CellTypes()
         {
@@ -76,41 +45,27 @@ namespace Entities
         }
     }
 
-    
-
     public class Category : Entity
     {
         public static new readonly int dimensions = 2;
         public int category_number { get; set; }
         public string category_name { get; set; }
+
         public override string ToString()
         {
             return $"Category: \n\acategory_number: {category_number}, \n\acategory_name: {category_name}";
         }
-
         public override List<string> ToList()
         {
             return new List<string> { category_number.ToString(), category_name };
         }
-        public static new string OrderQuery(string attr, bool desc = false)
-        {
-            return @$"
-            SELECT 
-                *
-            FROM
-                category
-            ORDER BY {attr} {(desc ? "DESC" : "ASC")}";
-        }
-        public override List<ColumnType> ColumnTypes()
-        {
-            return new List<ColumnType> { ColumnType.Editable, ColumnType.Editable };
-        }
-
         public static new List<CellType> CellTypes()
         {
             return new List<CellType> { CellType.InputField, CellType.InputField };
         }
     }
+
+
 
     //TODO: 
     public class Bill

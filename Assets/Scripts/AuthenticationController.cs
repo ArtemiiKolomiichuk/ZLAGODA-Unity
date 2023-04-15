95edd85e214df882ddb1e01dbebbbd6a52f9bf98
ArtemiiKolomiichuk <artemii.kolomiichuk@ukma.edu.ua>
2023-04-15 21:20:57 +0300

    Add draft authentication scene

---
+++ Assets/Scripts/AuthenticationController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AuthenticationController : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField loginInput;
+    [SerializeField] private TMP_InputField passwordInput;
+
+    [SerializeField] private Button loginButton;
+
+    private void Start()
+    {
+        loginButton.onClick.AddListener(() => Login());
+    }
+
+    private void Login()
+    {
+        //TODO: Add authentication logic
+        SceneManager.LoadScene("Menu");
+    }
+}
